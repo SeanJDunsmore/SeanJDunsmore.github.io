73dfbe7ba078cd8eecc9b9152bf5833b2fbacdbb
Sean <seanjdunsmore@gmail.com>
2026-03-11 07:29:01 +0000

    Added js file

---
+++ index.js
@@ -0,0 +1,13 @@
+
+	//java script to reset forms on load
+	 window.onload = function () {
+            // Reset the form fields when the page loads
+            document.getElementById("form").reset();
+        };
+
+	//Script to ensure youtube videos run on firefox
+	var tag = document.createElement('script');
+	tag.src = "//www.youtube.com/iframe_api";
+	var firstScriptTag = document.getElementsByTagName('script')[0];
+	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+	var url = window.location.pathname; 
